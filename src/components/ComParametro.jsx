07122ac1c5588811ca39ba69ab6c873db8571133
Eduardo <eduardo@Mac-mini-de-Eduardo.local>
2020-07-23 16:41:38 -0300

    First Commit

---
+++ src/components/ComParametro.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default param =>
+    <>
+        <h3>{param.titulo}</h3>
+        <p>{param.subtitulo}</p>
+    </>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <div>
+            <Primeiro></Primeiro>
+            <ComParametro titulo="Esse é o título"
+                subtitulo="Esse é o subtítulo"/>
+    </div>,
+    document.getElementById('root')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
